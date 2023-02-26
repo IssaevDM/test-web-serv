85b89a37a4873b6bf3024d5e462681037303a1b4
Evgeny Saveliev <evgeny.saveliev@brivo.com>
2023-02-26 09:15:46 -0500

    Add socket connection funcrtion

--- flask_docker/app.py
+++ flask_docker/app.py
@@ -7,5 +7,11 @@ app = flask.Flask(__name__)
 def index():
     return str(uuid.uuid4())
 
+@socket_.on('connect')
+def test_connect():
+    app.logger.critical("Connection message")
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my response', {'data': 'Connected\n', 'count': session['receive_count']})
+
 if __name__ == "__main__":
     app.run()
