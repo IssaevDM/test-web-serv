a5102a8f8b630b717c8b0c921f917caceb824172
IssaevDM <issaevdm@gmail.com>
2023-03-12 17:17:21 +0300

    added more info

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = flask.Flask(__name__)
 
 @app.route("/")
 def index():
-    return str(uuid.uuid4())
+    return str(f"our unique ID:{uuid.uuid4()}")
 
 if __name__ == "__main__":
     app.run()
