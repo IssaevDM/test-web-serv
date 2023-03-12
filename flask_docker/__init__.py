789a55a0fc7f8350486d7d49d4b3b6fea26dd7b5
IssaevDM <issaevdm@gmail.com>
2023-03-12 17:26:39 +0300

    redone init.py

--- flask_docker/__init__.py
+++ flask_docker/__init__.py
@@ -1,13 +1,6 @@
-import flask
-import uuid
+from flask_docker.app import app
 
 version = "0.1.0b"
-
-app = flask.Flask(__name__)
-
-@app.route("/")
-def index():
-    return str(uuid.uuid4())
 
 if __name__ == "__main__":
     app.run()
