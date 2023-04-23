4d9dd015ef03a89ce3573429eb4193bfa1426d7b
Evgeny Saveliev <evgeny.saveliev@brivo.com>
2023-04-23 12:04:22 -0400

    Designing game state JSON

--- flask_docker/game_logic.py
+++ flask_docker/game_logic.py
@@ -2,6 +2,27 @@ import sqllite3
 from attrs import define, field
 
 
+# game_state JSON
+# {
+#     "stack": ["KH", "AH", "2H", "3C", "5D", "{A|K|Q|J|10 ... 2}{H|D|C|S}", "Last card must coincide with trump suite"],
+#     "used_stack": ["Same as stack"],
+#     "trump_suite": "{H|D|C|S}",
+#     "table": [
+#         {
+#             "card": "",
+#             "owner": "UUID"
+#         }
+#     ],
+#     "attacker": "UUID",
+#     "defender": "UUID",
+#     "players": {
+#         "UUID": {
+#             "hand": ["not in stack or used stack"]
+#         }
+#     }
+# }
+
+
 @define(kw_only=True)
 class Game:
     id = field()
@@ -9,6 +30,5 @@ class Game:
     def saveState(self):
         with sqllite3.connect("durak.db") as sql3conn:
             with sql3conn.cursor() as cur:
-                
-
-        sqllite3.
+                query = f"""insert into games (id, game_state)"""
+                cur.execute(query)
