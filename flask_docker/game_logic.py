1106059dbe1ca3afb42c7ff68a9cb641a4ef9de2
IssaevDM <issaevdm@gmail.com>
2023-04-23 19:32:53 +0300

    game logic: added SaveState

--- flask_docker/game_logic.py
+++ flask_docker/game_logic.py
@@ -1,4 +1,5 @@
-import sqllite3
+import sqlite3
+import json
 from attrs import define, field
 
 
@@ -26,9 +27,26 @@ from attrs import define, field
 @define(kw_only=True)
 class Game:
     id = field()
-    
+    stack = field(init=False, factory=list)
+    used_stack = field(init=False, factory=list)
+    trump_suite = field(init=False, default="")
+    table = field(init=False, factory=list)
+    attacker = field(init=False, default=None)
+    defender = field(init=False, default=None)
+    players =  field(init=False, factory=dict)
+
     def saveState(self):
-        with sqllite3.connect("durak.db") as sql3conn:
+        with sqlite3.connect("durak.db") as sql3conn:
             with sql3conn.cursor() as cur:
-                query = f"""insert into games (id, game_state)"""
+                game_state = {
+                    "stack": self.stack,
+                    "used_stack": self.used_stack,
+                    "trump_suite": self.trump_suite,
+                    "table": self.table,
+                    "attacker": self.attacker,
+                    "defender": self.defender,
+                    "players": self.players,
+                }
+                query = f"""insert into game_states (id, game_state) values ({self.id}, '{json.dumps(game_state)}') on conflict replace"""
                 cur.execute(query)
+            
